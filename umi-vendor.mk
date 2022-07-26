ffeb8e65bfbfca1dd228bf187518e95bd555c703
Alexander Koskovich <akoskovich@pm.me>
2022-07-26 22:41:50 +0800

    umi: Drop updatable GPU drivers
     * Doesn't work, can't switch between default and Xiaomi
       GPU driver. Looking at Xiaomi's APK, there is nothing
       really inside just a shell. There were no updates for
       the app in the Play Store either.
    
    Change-Id: I0ec61c3b9abfa86368e6c23481d7edf5d8f6a036

--- umi-vendor.mk
+++ umi-vendor.mk
@@ -1348,7 +1348,6 @@ PRODUCT_PACKAGES += \
     IWlanService \
     SoterService \
     TimeService \
-    com.qualcomm.qti.gpudrivers.kona.api30 \
     PowerOffAlarm \
     HotwordEnrollmentOKGoogleHEXAGON \
     HotwordEnrollmentXGoogleHEXAGON \
